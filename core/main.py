5f019dc86214d03f28efa373185202f9a1c8c477
Vazirified <vaziri@zoho.com>
2023-05-25 18:31:15 +0300

    The program now reads configuration from an external file (at this stage the only config read is the server_url) and retrieves credentials from OS keychain to establish a connection with the remote CalDAV server.

--- core/main.py
+++ core/main.py
@@ -1,6 +1,19 @@
+# TODO: Imports should be optimized after we determine what components we are using from the libraries.
 import vobject
 import caldav
 import os
 import hashlib
+import keyring
 
+# Configuration variables are imported/accessed.
+# TODO: Change code to manage server url in a local configuration.py file.
+from configuration import *
 
+# Credentials are retrieved from OS keychain and used to establish a connection/session with the remote CalDAV server.
+# TODO: Write code to manage username and password of the CalDAV server in they OS keychain through keyring. The remote
+#  username/password are saved to ("priorg-caldav", "username", "password") and the username can be accessed from
+#  ("priorg-caldav", "priorg", "username").
+client_session = caldav.DAVClient(url=server_url,
+                                  username=keyring.get_password("priorg-caldav", "priorg"),
+                                  password=keyring.get_password("priorg-caldav", keyring.get_password("priorg-caldav",
+                                                                                                      "priorg")))
