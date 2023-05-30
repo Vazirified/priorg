2a64ddf21fb7171305736e77bc359bac73657841
Vazirified <vaziri@zoho.com>
2023-05-30 19:01:38 +0300

    We now have a module to make sure data directory and constant structure inside it exists and created if not.

---
+++ core/pathcheck.py
@@ -0,0 +1,15 @@
+import os
+from configuration import *
+
+def directory_check(path_to_check):
+    if not os.path.exists(path_to_check):
+        os.makedirs(path_to_check, exist_ok=True)
+
+def file_check(path_to_check, file_to_check):
+    if not os.path.exists(path_to_check + file_to_check):
+        open(path_to_check + file_to_check, 'x').close()
+
+directory_check(local_files_path)
+file_check(local_files_path, "server_todo_hashes.json")
+file_check(local_files_path, "local_todo_hashes.json")
+file_check(local_files_path, "synced_todo_hashes.json")
