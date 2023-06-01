601c21ee8b8da6f1d8591faa564bdfff8051dbe8
Vazirified <vaziri@zoho.com>
2023-06-01 17:05:12 +0300

    file_check function changed to become completely dependent on passed parameters.

--- core/pathcheck.py
+++ core/pathcheck.py
@@ -1,4 +1,3 @@
-# TODO: Integrate this module with synchronizer.py if not used anywhere else.
 import os
 from configuration import *
 
@@ -6,12 +5,13 @@ def directory_check(path_to_check):
     if not os.path.exists(path_to_check):
         os.makedirs(path_to_check, exist_ok=True)
 
-def file_check(path_to_check, file_to_check):
+# This function checks for existence of the given file at the given path and creates it with default contents if not.
+def file_check(path_to_check, file_to_check, contents):
     if not os.path.exists(path_to_check + file_to_check):
-        with open(path_to_check + file_to_check, 'w') as hashfile:
-            hashfile.write("{}")
+        with open(path_to_check + file_to_check, "w") as hashfile:
+            hashfile.write(contents)
 
 directory_check(local_files_path)
-# file_check(local_files_path, "server_todo_hashes.json")
-# file_check(local_files_path, "local_todo_hashes.json")
-file_check(local_files_path, "synced_todo_hashes.json")
+# file_check(local_files_path, "server_todo_hashes.json", "{}")
+# file_check(local_files_path, "local_todo_hashes.json", "{}")
+file_check(local_files_path, "synced_todo_hashes.json", "{}")
