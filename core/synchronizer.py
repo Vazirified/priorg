74c4a8d7124e0174fcaa53300e574020e5f29065
Vazirified <vaziri@zoho.com>
2023-06-14 18:54:08 +0300

    Synchronizer now finds and deletes the VTODO items that are deleted locally, from the server.

--- core/synchronizer.py
+++ core/synchronizer.py
@@ -76,30 +76,41 @@ for uid_item in no_dup_uids:
         # This means the item exists in all dictionaries. We now have to check the hash and see if it is changed on
         # any side...
         pass
+
     elif uid_item in server_todo_hashes and uid_item not in local_todo_hashes and uid_item not in synced_todo_hashes:
         # This item is only stored on the sever. It must have been created on the server in the interval between current
         # synchronization and the previous one. Such items must be created in the local copies.
         pass
+
     elif uid_item not in server_todo_hashes and uid_item in local_todo_hashes and uid_item not in synced_todo_hashes:
         # This item only exists locally. It must have been created locally between this synchronization and the previous
         # one. Such items must be created on the server.
         pass
+
     elif uid_item not in server_todo_hashes and uid_item not in local_todo_hashes and uid_item in synced_todo_hashes:
         # This item only exists in the synced list. This means that it was present everywhere during the last
         # synchronization but was deleted from both sides in the interval (which is a bit strange but can happen).
         # Such an item does not require any action. It will be deleted from the synced items dictionary at the end of
         # current synchronization automatically. We also need to issue a warning.
         pass
+
     elif uid_item in server_todo_hashes and uid_item in local_todo_hashes and uid_item not in synced_todo_hashes:
         # Item exists on server and locally, but it was not present in previous synchronization. This means that the
         # is miraculously created on both sides with the same UID between this sync and the last one.
         # We have to check the hashed on both sides and if not equal, the one with newer modification date must be
         # copied to the other side. We also need to issue a warning for this.
         pass
+
     elif uid_item in server_todo_hashes and uid_item not in local_todo_hashes and uid_item in synced_todo_hashes:
         # Item is on the server and was present after the previous synchronization, but does not exist locally. Such an
         # item must have been deleted locally between the two synchronizations and must be deleted from the server too.
-        pass
+        for todo in server_todos:
+            print(todo)
+            working_todo_uid = str(todo.instance.vtodo.uid)
+            working_todo_uid_parsed = working_todo_uid[working_todo_uid.find("}") + 1: working_todo_uid.find(">")]
+            if working_todo_uid_parsed == uid_item:
+                todo.delete()
+
     elif uid_item not in server_todo_hashes and uid_item in local_todo_hashes and uid_item in synced_todo_hashes:
         # Item exists locally and was present after the previous synchronization, but is not on the server. Such an
         # item must have been deleted from the server between the two synchronizations and must be deleted locally too.
@@ -112,6 +123,7 @@ for uid_item in no_dup_uids:
                                               working_todo_uid.find("}") + 1: working_todo_uid.find(">")]
                     if working_todo_uid_parsed == uid_item:
                         os.remove(local_files_path + file)
+
     else:
         # This situation means that something has gone wrong as it is impossible to happen! This "else" statement should
         # not really exist! But let's include it for now and raise some kind of error if this happens...
@@ -143,3 +155,6 @@ with open(local_files_path + 'synced_todo_hashes.json', 'w') as working_file:
 
 # Connection/session with server is closed.
 server_session.close()
+
+for synced_item in synced_todo_hashes:
+    print(synced_item, ">>>", synced_todo_hashes[synced_item])
