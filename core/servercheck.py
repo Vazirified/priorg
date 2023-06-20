818709fdf99b7621ced5698d57213d11e92cd218
Vazirified <vaziri@zoho.com>
2023-06-20 13:12:12 +0300

    A new todo added to remind to write the server check procedures.

---
+++ core/servercheck.py
@@ -0,0 +1,4 @@
+# TODO: Write the servercheck module to make sure the remote server accepts VTODO items using get_supported_compunents
+#  from CalDAV library:
+#  acceptable_component_types = calendar.get_supported_components()
+#  assert "VTODO" in acceptable_component_types
